4a0f9e21f1d46c7f3712f1507abcf1005a357a0c
toelapiut <toelapiut7@gmail.com>
2018-01-17 16:36:07 +0300

    (update) information of the dashboard

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import FlashMessagesList from './components/FlashMessagesList';
 import requireAuth from './utils/requireAuth';
-
+import Error404 from './components/Error404';
 class App extends Component {
   render() {
     return (
@@ -19,6 +19,7 @@ class App extends Component {
           <Route exact path='/' component={LoginPage}/>
           <Route exact path='/signup' component={SignUpPage}/>
           <Route exact path='/dashboard' component={requireAuth(Home)}/>
+          <Route component={Error404} />
         </Switch>
       </div>
     );
