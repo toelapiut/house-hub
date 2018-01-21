0e6463c4a61e44566cbc3f12732d70492451e1b5
David Mwangi <njorogedavid13@gmail.com>
2018-01-21 20:16:20 +0300

    feat(data from api to tenants table component):

--- src/components/dashboardCharts/SomeTenants.jsx
+++ src/components/dashboardCharts/SomeTenants.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default class SomeTenants extends React.Component {
     render(){
+        const tenantData = this.props.tenantData
+        // .map((tenant) => {
+        //     return 
+        //         // <h1>{todo.title}</h1>
+        //         <tbody>
+        //         <tr>
+        //             <td>{tenant.property}</td>
+        //             <td>3A</td>
+        //             <td>{tenant.name}</td>
+        //             <td>{tenant.phone_number}</td>
+        //         </tr>
+        //         </tbody>
+        // })
+        // let todos = this.props.todos.map((todo) => {return <h1>{todo.title}</h1>})
+        	
+        console.log(tenantData)
         return(
             <table className="table table-striped">
                 <thead>
@@ -19,18 +35,16 @@ export default class SomeTenants extends React.Component {
                         <td>David Mwangi</td>
                         <td>Paid</td>
                     </tr>
-                    <tr>
-                        <td>Court Peaky</td>
-                        <td>2A</td>
-                        <td>Toel Apiut</td>
-                        <td>Not paid</td>
-                    </tr>                    
-                    <tr>
-                        <td>Court Peaky</td>
-                        <td>1A</td>
-                        <td>Nish Ndungu</td>
-                        <td>Paid</td>
-                    </tr>
+                    {tenantData.map((item)=>
+
+                        <tr>
+                            <td>{item.property}</td>
+                            <td>3A</td>
+                            <td>{item.name}</td>
+                            <td>{item.phone_number}</td>
+                        </tr>
+
+                    )}
                 </tbody>
             </table>
         )
